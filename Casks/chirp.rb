6d31b8ce37689a1fc940c31ad4b8cd65a74d35aa
Jeffrey C Honig <jch@honig.net>
2026-01-27 04:30:17 -0500

    Update Chirp to 20260123

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
   version "20260123"
-  sha256 "983e217446c7134ef8ea0cd189b5f8ee148db352787aa3623dc884e45104b3fb"
+  sha256 "841a4c2e2792f797da0a578fd0ca0f39cbfa57e3206763916b092fc76d14334c"
 
   url "https://archive.chirpmyradio.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip",
       verified: "archive.chirpmyradio.com/",
