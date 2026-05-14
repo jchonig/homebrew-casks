6456d759d29cc6e52b09660e00958b6e33d8f5d3
Jeffrey C Honig <jch@honig.net>
2026-05-14 15:39:24 -0400

    Fix style for HamClockLauncher

--- Casks/hamclocklauncher.rb
+++ Casks/hamclocklauncher.rb
@@ -1,19 +1,22 @@
 cask "hamclocklauncher" do
   version "1.4"
 
+  on_arm do
+    sha256 "20126f764c9ed6d627c62a17453a3717173b6256f6469fc77d31ad622fb5770f"
+
+    url "https://github.com/huberthickman/HamClockLauncher/releases/download/release_#{version.dots_to_underscores}/HamClockLauncher.dmg"
+  end
+  on_intel do
+    sha256 "0eae265bea9971ef536402774aea155e8e7dbfa81f7630b9e12c3745815914be"
+
+    url "https://github.com/huberthickman/HamClockLauncher/releases/download/release_#{version.dots_to_underscores}/HamClockLauncherIntel.dmg"
+  end
+
   name "HamClock Launcher"
-  desc "macOS GUI launcher for the HamClock web application"
+  desc "GUI launcher for the HamClock web application"
   homepage "https://github.com/huberthickman/HamClockLauncher"
 
-  on_intel do
-    url "https://github.com/huberthickman/HamClockLauncher/releases/download/release_#{version.dots_to_underscores}/HamClockLauncherIntel.dmg"
-    sha256 "0eae265bea9971ef536402774aea155e8e7dbfa81f7630b9e12c3745815914be"
-  end
-
-  on_arm do
-    url "https://github.com/huberthickman/HamClockLauncher/releases/download/release_#{version.dots_to_underscores}/HamClockLauncher.dmg"
-    sha256 "20126f764c9ed6d627c62a17453a3717173b6256f6469fc77d31ad622fb5770f"
-  end
+  depends_on :macos
 
   app "HamClockLauncher.app"
 
